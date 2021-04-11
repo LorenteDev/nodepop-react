64cb382c9b0fa593122034597d4bbca07682a3fc
LorenteDev <74774447+LorenteDev@users.noreply.github.com>
2021-04-11 12:36:26 +0200

    Cambio de imagenes locales

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,5 +1,23 @@
+import Navbar from "../components/Nav/Navbar/Navbar";
+import NotFoundImage from "../assets/notFound.gif";
+import { useHistory } from "react-router-dom";
+
 const NotFound = () => {
-  return <h1>NotFound</h1>;
+  const history = useHistory();
+
+  const goHome = () => {
+    history.push("/");
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="text-center">
+        <h1>404 | Not found</h1>
+        <img src={NotFoundImage} alt="Not found" onClick={goHome} />
+      </div>
+    </>
+  );
 };
 
 export default NotFound;
